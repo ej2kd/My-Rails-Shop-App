23acfea16c0c9249de4f25450d43ee49d08c82e6
ej2kd <eiji.tsukada9415@gmail.com>
2021-11-24 00:56:05 +0900

    add image_upload feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -64,6 +64,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:id, :name, :price, :barcode, :description)
+      params.require(:item).permit(:id, :name, :item_image, :price, :barcode, :description)
     end
 end
